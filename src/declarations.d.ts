e7c5bdb43bb50d62b339cb9dcba21712a651309d
Jakub Synowiec <github@jakubsynowiec.info>
2017-03-31 21:04:43 +0200

    Migrate extension to TypeScript

---
+++ src/declarations.d.ts
@@ -0,0 +1,8 @@
+// ambient module declaration for https://www.npmjs.com/package/date-format-lite
+declare module 'date-format-lite' { }
+
+// date-format-lite augments built-in Date
+interface Date {
+  format: (format?: string) => string;
+  masks: { default: string };
+}
---
+++ src/extension.ts
@@ -0,0 +1,53 @@
+import { commands, workspace, window, ExtensionContext } from 'vscode';
+
+import 'date-format-lite';
+
+const INPUT_PROMPT = 'Date and Time format';
+const DEFAULT_FORMAT = 'YYYY-MM-DD hh:mm:ss';
+
+function getConfiguredFormat(): string {
+  const insertDateStringConfiguration = workspace.getConfiguration('insertdatestring');
+  return insertDateStringConfiguration.get('format', DEFAULT_FORMAT);
+}
+
+function getFormattedDateString(userFormat = getConfiguredFormat()): string {
+  const now = new Date();
+  return now.format(userFormat);
+}
+
+function replaceEditorSelection(text: string) {
+  const editor = window.activeTextEditor;
+  const selections = editor.selections;
+
+  editor.edit((editBuilder) => {
+    selections.forEach((selection) => {
+      editBuilder.replace(selection, '');
+      editBuilder.insert(selection.active, text);
+    });
+  });
+}
+
+export function activate(context: ExtensionContext): void {
+  context.subscriptions.push(commands.registerCommand(
+    'insertdatestring.insertdatetime',
+    () => replaceEditorSelection(getFormattedDateString())
+  ));
+
+  context.subscriptions.push(commands.registerCommand(
+    'insertdatestring.inserttimestamp',
+    () => replaceEditorSelection((new Date()).getTime().toString())
+  ));
+
+  context.subscriptions.push(commands.registerCommand(
+    'insertdatestring.insertownformatdatetime',
+    () => {
+      window.showInputBox({
+        value: getConfiguredFormat(),
+        prompt: INPUT_PROMPT,
+      }).then((format) => {
+        replaceEditorSelection(getFormattedDateString(format));
+      });
+    }
+  ));
+
+};
